d6af13053f2d1629f102694d4ab338392c56fafd
Alexander <alexanderschwarz.ulm@gmail.com>
2022-01-17 11:34:47 +0100

    added required packages to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='gym_recording',
+setup(name='gym-dataset-recorder',
       version='0.0.1',
-      install_requires=['gym'],
+      install_requires=['gym', 'numpy', 'pandas', 'pyglet'],
       packages=find_packages(),
 )
